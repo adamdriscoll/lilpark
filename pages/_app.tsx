f0c2428b87f635b1154003ca1a3a73278a97908e
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2022-07-20 13:45:38 -0600

    cf

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return [<Head>
     <title>Little Park</title>
-  </Head>, <Component {...pageProps} />]
+  </Head>, <Component {...pageProps} />, <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "2b7391fd755e4282a739d4f1226db49f"}'></script>]
 }
 
 export default MyApp
