f7224530bdebafac4e22e129b18cfbfd3f2841d3
Adam Driscoll <adam.r.driscoll@gmail.com>
2022-07-20 21:22:55 -0600

    not gonna work.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,9 @@ import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  return [<Head>
+  return [<Head key={1}>
     <title>Little Park</title>
-  </Head>, <Component {...pageProps} />, <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "2b7391fd755e4282a739d4f1226db49f"}'></script>]
+  </Head>, <Component key={2} {...pageProps} />, <script key={3} defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "2b7391fd755e4282a739d4f1226db49f"}'></script>]
 }
 
 export default MyApp
