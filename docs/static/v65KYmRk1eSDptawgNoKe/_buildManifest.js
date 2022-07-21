c74b9c89e78d60bfbe051c4f370cd261f7dbac26
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2022-07-21 13:27:30 -0600

    Create react app build.

--- docs/static/v65KYmRk1eSDptawgNoKe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/6656e8de-cf6b7e8699726cee.js","static/chunks/784-1cc91ed3b7cad036.js","static/chunks/pages/index-b00bc9232f7a7be3.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
